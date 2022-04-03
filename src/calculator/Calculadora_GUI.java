619d5a5de7616de716f9cd93159e53397baa4ec9
Archerd6 <archerd@uma.es>
2022-04-03 13:53:32 +0200

    Solucion de errores *- /-

--- src/calculator/Calculadora_GUI.java
+++ src/calculator/Calculadora_GUI.java
@@ -666,7 +666,7 @@ public class Calculadora_GUI {
 					if(!Operacion.equals("+") && !Operacion.equals("-") && !Operacion.equals("*") && !Operacion.equals("/"))
 					{
 						// Combinado *- /-
-						Operacion =EntradaBuilder.subSequence(EntradaBuilder.lastIndexOf(arrayNumeros[1])-3,EntradaBuilder.lastIndexOf(arrayNumeros[1])-1).toString();;
+						Operacion =EntradaBuilder.subSequence(EntradaBuilder.lastIndexOf("-")-1,EntradaBuilder.lastIndexOf("-")+1).toString();;
 					}
 					Double resultado = 0.0;
 					Double uno = Double.parseDouble(arrayNumeros[0]);
